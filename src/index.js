5652b7df24da9abe0411e424541ef235e19c4375
rrmini <roman@transkontinent.net>
2019-11-28 14:48:38 +0300

    sign Up

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ import "typeface-roboto";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import Firebase, { FirebaseContext } from "./components/Firebase";
+
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
